89331ec16b944a3cc249ec87745ec05f087150ea
Jeong-ki <7863z@naver.com>
2023-11-11 18:25:16 +0900

    eslint, prettier setting

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "eslint-config-airbnb-base",
+    "eslint-config-prettier",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "arrow-parens": ["error", "as-needed"],
+    "@typescript-eslint/no-unused-vars": "off",
+    "no-unused-vars": "off",
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  // 화살표 함수 매개변수 1개일 때 괄호 빼줌.
+  arrowParens: "avoid",
+  parser: "typescript",
+};
